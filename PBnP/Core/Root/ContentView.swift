73cd0ca3ad8033f3ba98599956b8e7f3b105cbb3
stringtheory92 <adamkabak@gmail.com>
2024-04-01 15:08:57 -0400

    filetree reorg

---
+++ PBnP/Core/Root/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PBnP
+//
+//  Created by Adam Kabak on 4/1/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                Text("Hello, world")
+        }
+    }
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
